607fa0bf6cdc83ffae1df56ecfa6ebe28f202ce4
chaeyoung <happy7yong83@gmail.com>
2025-05-07 17:56:44 +0900

    fix : State, props를 이용하여 컴포넌트 분리하기

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,21 +1,12 @@
 import "./App.css";
-import Header from "./Components/Header.jsx";
-import Footer from "./Components/Footer.jsx";
-import Main from "./Components/Main.jsx";
-import Button from "./Components/Button.jsx";
+import Bulb from "./Components/Bulb.jsx";
+import Counter from "./Components/Counter.jsx";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-  };
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-      </Button>
+      <Bulb />
+      <Counter />
     </>
   );
 }
