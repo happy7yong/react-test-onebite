a9a5093d29534b6c941e954ed391d7d4904cbefb
chaeyoung <happy7yong83@gmail.com>
2025-05-12 18:48:13 +0900

    feat : useState 위치를 App 부모 컴포넌트로 변경

--- section05/Counter/src/App.jsx
+++ section05/Counter/src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import Viewer from "./components/Viewer.jsx";
+import Controller from "./components/Controller.jsx";
+import { useState } from "react";
 
 function App() {
-  return <>카운터앱</>;
+  const [count, setCount] = useState(0);
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
